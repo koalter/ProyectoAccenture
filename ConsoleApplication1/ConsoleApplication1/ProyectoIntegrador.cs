7a98925388c9c19d0d185541a5a28a4c587135f9
FacuDePaul <facundodepaul@gmail.com>
2018-08-02 11:59:06 -0300

    agregadad clase proyectointegrador

---
+++ ConsoleApplication1/ConsoleApplication1/ProyectoIntegrador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ProyectoIntegrador
+    {
+        const string nombreAdministrador = "";
+
+        public ProyectoIntegrador()
+        {
+
+        }
+        public string Authentication(string nom)
+        {
+            string nombre = nom;
+            // aca se pregunta el nombre del empleado
+            return nombre;
+        }
+        void OnAccess(string nombre)
+        {
+            if (nombre == nombreAdministrador)
+            {
+
+            }
+
+        }
+        void Deinit()
+        {
+
+        }
+        public void Clear()
+        {
+            Console.Clear();
+        }
+    }
+}
