20f96bd4059fe8aa19771555c6d784fa4e556748
lucasmasche <lucasmasche@hotmail.com>
2018-08-02 11:45:50 -0300

    pagina de inicio

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.Title = "Comercio Cualquiera";
+            Console.WriteLine("¿Qué usuario es?");
+            Console.WriteLine("1. Administrador");
+            Console.WriteLine("2. Empleado");
+            int op;
+            if(int.TryParse(Console.ReadLine(), out op))
+            {
+                Console.WriteLine(op);
+            }
+            else
+            {
+                Console.WriteLine("Ingreso una opción incorrecta");
+            }
+            Console.ReadKey();
         }
     }
 }
